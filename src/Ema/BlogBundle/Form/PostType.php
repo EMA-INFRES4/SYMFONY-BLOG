2493573eb69e1a63110a6f920633a1f78916fca5
Xavier TALANDIER <xavier@talandier.fr>
2013-10-07 15:41:29 +0200

    CRUD

---
+++ src/Ema/BlogBundle/Form/PostType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Ema\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PostType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('urlAlias')
+            ->add('content')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Ema\BlogBundle\Entity\Post'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ema_blogbundle_post';
+    }
+}
